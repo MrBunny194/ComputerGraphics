8ec21393b98dbb7022979a5288cf26051a4aec99
Nikita <nikitavolobuev@outlook.com>
2017-06-03 18:42:52 +0300

    update

--- BresenhamLineAlgorithm/Properties/AssemblyInfo.cs
+++ BresenhamLineAlgorithm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComputerGraphics")]
+[assembly: AssemblyTitle("BresenhamLineAlgorithm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComputerGraphics")]
+[assembly: AssemblyProduct("BresenhamLineAlgorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
